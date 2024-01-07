09807041894700d4f5e26ccec79ebe78b1df26b4
elhamgoudarzi6 <elhamgoudarzi6@gmail.com>
2024-01-08 00:52:39 +0330

    1402/10/17

---
+++ src/app/layout/order/order.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MenuItem, MessageService } from 'primeng/api';
+
+@Component({
+  selector: 'app-order-steps',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.scss'],
+  providers: [MessageService]
+})
+export class OrderComponent {
+  items: MenuItem[] | any;
+  constructor(public messageService: MessageService) { }
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'اطلاعات مشتری',
+        icon: 'pi pi-user',
+        routerLink: 'userinfo'
+      },
+      {
+        label: 'فرم سفارش',
+        icon: 'pi pi-shopping',
+        routerLink: 'orderinfo'
+      },
+    ];
+  }
+
+}
